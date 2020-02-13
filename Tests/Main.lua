b51cb7fdcc60d5d951c4b896846ceebde8203091
Vasilii Filippov <vasja.filippov94@outlook.com>
2020-02-14 02:27:06 +0300

    Added 2nd player

--- Tests/Main.lua
+++ Tests/Main.lua
@@ -6,7 +6,10 @@ local Core = Require("Core.Core")
 
 local testHeroPreset = DuskKnight()
 local core = Core(WCPlayer.Get(8), 0, -1800, 0)
-local testHero = testHeroPreset:Spawn(WCPlayer.Get(0), 0, -1600, 0)
+
+for i = 0,1 do
+    testHeroPreset:Spawn(WCPlayer.Get(i), 0, -1600, 0)
+end
 
 local testWaveObserver = WaveObserver(WCPlayer.Get(9))
 
