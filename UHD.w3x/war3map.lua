82dea8da9dabd1cc5536bc02eb308d7b8c860214
Andrey <andriei.shtyrov@gmail.com>
2020-02-17 03:22:18 +0300

    Add log to creep

--- UHD.w3x/war3map.lua
+++ UHD.w3x/war3map.lua
@@ -422,6 +422,11 @@ local Class = require("Class")
 local UHDUnit = require("Core.UHDUnit")
 local Timer = require("WC3.Timer")
 local Creep = Class(UHDUnit)
+local Log = require("Log")
+local creepLog = Log.Category("CreepSpawner\\CreepSpawnerr", {
+    printVerbosity = Log.Verbosity.Trace,
+    fileVerbosity = Log.Verbosity.Trace,
+    })
 
     function Creep:Destroy()
         local timer = Timer()
@@ -433,6 +438,7 @@ local Creep = Class(UHDUnit)
 
     function Creep:Scale(level, heroesCount)
         local mult = (1 + 0.05 * (0.7 * heroesCount +  0) * (level - 1))
+        creepLog:Info("multiplier "..mult)
         self.secondaryStats.health = mult * self.secondaryStats.health
         self.secondaryStats.physicalDamage = mult * self.secondaryStats.physicalDamage
         self.secondaryStats.armor = mult * self.secondaryStats.armor
