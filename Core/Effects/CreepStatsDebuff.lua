2cf768f9affe493b342eb28d5f13c786807dfcea
Vasilii Filippov <vasja.filippov94@outlook.com>
2020-03-01 22:18:56 +0300

    Timed effect system

---
+++ Core/Effects/CreepStatsDebuff.lua
@@ -0,0 +1,32 @@
+local Class = require "Class"
+local CCEffect = require "Core.Effects.CCEffect"
+local Stats = require "Core.Stats"
+
+local CreepStatsDebuff = Class(CCEffect)
+
+function CreepStatsDebuff:ctor(stats, target, duration)
+    self.stats = stats
+    CreepStatsDebuff.ctor(self, target, duration)
+end
+
+function CreepStatsDebuff:OnStart()
+    for k, v in pairs(self.stats) do
+        if Stats.Secondary.adding[k] then
+            self.target.secondaryStats[k] = self.target.secondaryStats[k] + v
+        else
+            self.target.secondaryStats[k] = self.target.secondaryStats[k] * v
+        end
+    end
+end
+
+function CreepStatsDebuff:OnEnd()
+    for k, v in pairs(self.stats) do
+        if Stats.Secondary.adding[k] then
+            self.target.secondaryStats[k] = self.target.secondaryStats[k] - v
+        else
+            self.target.secondaryStats[k] = self.target.secondaryStats[k] / v
+        end
+    end
+end
+
+return CreepStatsDebuff
---
+++ Core/Effects/TimedEffect.lua
@@ -0,0 +1,28 @@
+local Class = require "Class"
+local WC3 = require "WC3.All"
+
+local TimedEffect = Class()
+
+function TimedEffect:ctor(target, duration)
+    self.timer = WC3.Timer()
+    self.timer:Start(duration, false, function() self:Destroy() end)
+    self.target = target
+    target.effects[self] = true
+    self:OnStart()
+end
+
+function TimedEffect:OnStart()
+    error("TimedEffect:OnStart not implemented. target: " .. self.target:GetName())
+end
+
+function TimedEffect:OnEnd()
+    error("TimedEffect:OnEnd not implemented: " .. self.target:GetName())
+end
+
+function TimedEffect:Destroy()
+    self:OnEnd()
+    self.timer:Destroy()
+    self.target.effects[self] = nil
+end
+
+return TimedEffect
