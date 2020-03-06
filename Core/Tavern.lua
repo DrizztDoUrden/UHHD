b220d1a13645f37dfc2aa58dc7421523ffd5026a
Vasilii Filippov <vasja.filippov94@outlook.com>
2020-03-07 01:50:23 +0300

    Updated tavern to pan camera and select hero

--- Core/Tavern.lua
+++ Core/Tavern.lua
@@ -9,11 +9,8 @@ local heroSpawnY = -3400
 
 local Tavern = Class(WC3.Unit)
 
-
 function Tavern:ctor(owner, x, y, facing, heroPresets)
     WC3.Unit.ctor(self, owner, FourCC("n000"), x, y, facing)
-
-    -- heroPresets[1]:Spawn(WC3.Player.Get(8), heroSpawnX, heroSpawnY, 0)
 
     self.owner = owner
     self.heroPresets = heroPresets
@@ -27,19 +24,24 @@ function Tavern:AddTrigger()
     trigger:AddAction(function()
         local buying = WC3.Unit.GetBying()
         local sold = WC3.Unit.GetSold()
-        local whichOwner = sold:GetOwner()
+        local owner = sold:GetOwner()
         local id = sold:GetTypeId()
-        logTavern:Trace("Unit bought with id "..id)
-        for _, hero in pairs(self.heroPresets) do
-            if hero.unitid == id then
-                hero:Spawn(whichOwner, heroSpawnX, heroSpawnY, 0)
+        local hero
+        logTavern:Trace("Unit bought with id ".. id)
+        for _, preset in pairs(self.heroPresets) do
+            if preset.unitid == id then
+                hero = preset:Spawn(owner, heroSpawnX, heroSpawnY, 0)
                 break
             end
         end
         buying:Destroy()
         sold:Destroy()
+            WC3.Camera.PanTo(heroSpawnX, heroSpawnY, owner)
+        if owner == WC3.Player.Local then
+            ClearSelection()
+            hero:Select()
+        end
     end)
 end
-
 
 return Tavern
