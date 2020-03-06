7ff8b7786ab063c1838ce48f1e2de32499005508
Vasilii Filippov <vasja.filippov94@outlook.com>
2020-03-07 00:41:44 +0300

    Implemented scripted spell cast target selection

--- Core/Spell.lua
+++ Core/Spell.lua
@@ -4,24 +4,34 @@ local WC3 = require "WC3.All"
 
 local Spell = Class()
 
-function Spell:ctor(definition, caster)
+function Spell:ctor(definition, caster, target)
     self.caster = caster
     for k, v in pairs(definition.params or {}) do
         self[k] = v(definition, caster)
     end
+    self.target = target or {}
     self:Cast()
 end
 
 function Spell:GetTargetUnit()
-    return WC3.Unit.GetSpellTarget()
+    if self.target.unit == nil then
+        self.target.unit = WC3.Unit.GetSpellTarget()
+    end
+    return self.target.unit
 end
 
 function Spell:GetTargetX()
-    return GetSpellTargetX()
+    if self.target.x == nil then
+        self.target.x = GetSpellTargetX()
+    end
+    return self.target.x
 end
 
 function Spell:GetTargetY()
-    return GetSpellTargetY()
+    if self.target.y == nil then
+        self.target.y = GetSpellTargetY()
+    end
+    return self.target.y
 end
 
 return Spell
